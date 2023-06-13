531eecb7681eea1b9734de9e9a46f1ba98ea99d2
psofea01 <136260800+psofea01@users.noreply.github.com>
2023-06-14 02:19:54 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,19 +28,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-# # New section to display fruityvice response
-# streamlit.header("Fruityvice Fruit Advice!")
-# fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-# streamlit.write('The user entered ', fruit_choice)
-
-# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# # streamlit.text(fruityvice_response.json()) # just writes the data to the screen
-
-# # take the json version of the response and normalize it
-# fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# # output it the screen as a table
-# streamlit.dataframe(fruityvice_normalized)
-
 # create the repeatable code block (called a function)
 def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
@@ -76,9 +63,9 @@ if streamlit.button('Get Fruit Load List'):
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
 
-# # New section to display fruit load list response
-# add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
-# streamlit.write('Thanks for adding ', add_my_fruit)
+# New section to display fruit load list response
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
+streamlit.write('Thanks for adding ', add_my_fruit)
 
-# # Change word
-# my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+# Change word
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
